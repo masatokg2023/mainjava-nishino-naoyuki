4c3cb00503b8533afab7f53452578ca8a975b702
masatokg <masatokg@users.noreply.github.com>
2023-09-01 12:03:29 +0900

    確認です

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ public class Main{
     public static void main(String[] args){
         System.out.println("hello java");
         System.out.println("hello java２");
+        // どうして２回も出力したのですか
     }
 }
